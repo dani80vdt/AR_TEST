766c959bdf375febddf16f7f71db6a561ca25b8a
Daniele Bulgarelli <ITBULGARELLD@tetrapak.com>
2019-11-30 14:31:40 +0100

    change ev pos

--- js/events.js
+++ js/events.js
@@ -7,21 +7,6 @@ AFRAME.registerComponent('markerhandler', {
         const plusMarker = document.querySelector("#plus");
         const aEntity = document.querySelector("#gauge");
 
-        // every click, we make our model grow in size :)
-        minusMarker.addEventListener('click', function (ev, target) {
-            console.log("Click minus");
-            const scale = aEntity.getAttribute('scale');
-            Object.keys(scale).forEach((key) => scale[key] = scale[key] - 1);
-            aEntity.setAttribute('scale', scale);
-        });
-		
-        plusMarker.addEventListener('click', function (ev, target) {
-            console.log("Click plus");
-            const scale = aEntity.getAttribute('scale');
-            Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-            aEntity.setAttribute('scale', scale);
-        });
-
         markerSelector.addEventListener('click', function (ev, target) {
             console.log("Click markerSelector");
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
@@ -31,4 +16,21 @@ AFRAME.registerComponent('markerhandler', {
                 aEntity.setAttribute('scale', scale);
             }
         });
+
+        // every click, we make our model grow in size :)
+        minusMarker.addEventListener('click', function (ev, target) {
+            console.log("Click minus");
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = scale[key] *0.8);
+            aEntity.setAttribute('scale', scale);
+        });
+		
+        plusMarker.addEventListener('click', function (ev, target) {
+            console.log("Click plus");
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = scale[key] *1.2);
+            aEntity.setAttribute('scale', scale);
+        });
+
+       
 }});
