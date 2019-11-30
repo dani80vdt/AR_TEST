548538ee3545916d1381039b02dfc2a7fb8ed1b3
Daniele Bulgarelli <ITBULGARELLD@tetrapak.com>
2019-11-30 14:24:26 +0100

    remove control intersection

--- js/events.js
+++ js/events.js
@@ -10,22 +10,16 @@ AFRAME.registerComponent('markerhandler', {
         // every click, we make our model grow in size :)
         minusMarker.addEventListener('click', function (ev, target) {
             console.log("Click minus");
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] - 1);
-                aEntity.setAttribute('scale', scale);
-            }
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = scale[key] - 1);
+            aEntity.setAttribute('scale', scale);
         });
 		
         plusMarker.addEventListener('click', function (ev, target) {
             console.log("Click plus");
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
-            }
+            const scale = aEntity.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+            aEntity.setAttribute('scale', scale);
         });
 
         markerSelector.addEventListener('click', function (ev, target) {
